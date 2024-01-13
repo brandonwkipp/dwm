1ee94fea9316af0709e6eb558edb2cde5470c182
Brandon W. Kipp <brandonwkipp@gmail.com>
2024-01-13 00:13:52 -0600

    updated backlight commands to work with acpilight

--- config.h
+++ config.h
@@ -65,8 +65,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-static const char *brightdeccmd[] = { "xbacklight", "-dec", "5%", NULL };
-static const char *brightinccmd[] = { "xbacklight", "-inc", "5%", NULL };
+static const char *brightdeccmd[] = { "xbacklight", "-dec", "5", NULL };
+static const char *brightinccmd[] = { "xbacklight", "-inc", "5", NULL };
 
 static const char *volumemutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *volumedowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
